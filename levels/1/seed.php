2f11097a49561129b68bc17b2758c9a8fffd7365
Henrik Opel <hopel@opelbrothers.net>
2012-11-13 19:11:12 +0100

    Restructured seed.php to separate code from markup in preparation of JSON POST handling addition (implies fixing header() commands to actually work, as issued before other output now). [Issue(s) #25]

--- levels/1/seed.php
+++ levels/1/seed.php
@@ -1,3 +1,29 @@
+<?php
+  define('MODE', 0600);
+  define('FILENAME_SECRET', 'secret-combination.txt');
+  define('FILENAME_PASSWORD', 'password.txt');
+
+  $message = NULL;
+  if (!file_exists(FILENAME_PASSWORD)) {
+    file_put_contents(FILENAME_SECRET, crypt(rand()));
+    chmod(FILENAME_SECRET, MODE);
+
+    if (!empty($_POST['password'])) {
+      $password = $_POST['password'];
+      file_put_contents(FILENAME_PASSWORD, $password);
+      chmod(FILENAME_PASSWORD, MODE);
+      $message = 'Seeded password.txt with: ' . $password;
+    } else {
+      header("HTTP/1.1 400 Bad Request");
+      $message = 'You must specify a non empty password!';
+    }
+  }
+  else {
+    header("HTTP/1.1 409 Conflict");
+    $message = 'Guessing Game has already been seeded, please redeploy!';
+  }
+?>
+
 <html>
   <head>
     <title>Guessing Game Administration</title>
@@ -7,30 +33,9 @@
     <p>
       Seed the password to the next level below (secret combination is randomized)!
     </p>
-    <?php
-      $mode = 0600;
-      $secretFilename = 'secret-combination.txt';
-      $passwordFilename = 'password.txt';
-      if (!file_exists($passwordFilename)) {
-        file_put_contents($secretFilename, crypt(rand()));
-        chmod($secretFilename, $mode);
-
-        if (!empty($_POST['password'])) {
-          $password = $_POST['password'];
-          file_put_contents($passwordFilename, $password);
-          chmod($passwordFilename, $mode);
-          echo "<p>Seeded password.txt with:" .
-              " $password</p>";
-        } else {
-          header("HTTP/1.1 400 Bad Request");
-          echo "<p>You must specify a non empty password!</p>";
-        }
-      }
-      else {
-        header("HTTP/1.1 409 Conflict");
-        echo "<p>Guessing Game has already been seeded, please redeploy!</p>";
-      }
-    ?>
+    <p>
+      <?php echo $message; ?>
+    </p>
     <form action="#" method="POST">
       <p><input type="text" name="password"></p>
       <p><input type="submit" value="Seed!"></p>
